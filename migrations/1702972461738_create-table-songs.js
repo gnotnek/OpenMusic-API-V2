6544b4fd47f132ab10d5d97b18d69e859b8e579c
Dhista Ardiansyah <56432757+gnotnek@users.noreply.github.com>
2024-01-15 14:10:36 +0700

    Remove album_id foreign key reference in create-table-songs.js

--- migrations/1702972461738_create-table-songs.js
+++ migrations/1702972461738_create-table-songs.js
@@ -31,8 +31,6 @@ exports.up = pgm => {
         album_id: {
             type: 'VARCHAR(50)',
             notNull: false,
-            references: 'albums(id)',
-            onDelete: 'cascade',
         },
     });
 
